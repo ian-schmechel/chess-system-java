82b2391431f06a9d351d8581b02811c935b192d1
Ian Schmechel <ian.schmechelads@gmail.com>
2024-06-18 14:35:24 -0300

    Chess layer and printing the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,17 @@
 package boardgame;
 
 public class Piece {
+	
+	protected Position postion;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+	}
 
+	protected Board getBoard() {
+		return board;
+	}
+	
+	
 }
